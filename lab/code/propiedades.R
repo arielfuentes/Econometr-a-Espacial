0652857a09cac9ccb020bfcf409d2095a6587d44
Ariel Fuentes <ariel.fuentesdi@usach.cl>
2022-01-19 22:34:21 -0300

    models

--- lab/code/propiedades.R
+++ lab/code/propiedades.R
@@ -9,7 +9,7 @@ library(vip)
 propie <- read_xlsx("lab/data/data.xlsx")
 summary(propie)
 propie <- filter(propie, !is.na(latitud) & longitud > -80 & longitud < -70 & latitud < -34) %>%
-  st_as_sf(coords = c("longitud", "latitud"), crs = 4326) %>% 
+  # st_as_sf(coords = c("longitud", "latitud"), crs = 4326) %>% 
   mutate(bien_casa = if_else(tipo_bien == "Casa", 1, 0))
 map <- tm_shape(propie) +
   tm_dots(col = "tot_constr_m2")
@@ -19,7 +19,7 @@ tmap_leaflet(map)
 ggpairs(select_if(st_drop_geometry(propie), is.numeric))
 ##############probar modelo lineal
 set.seed(123)
-dt_split <- initial_split(st_drop_geometry(propie), p = 3/4, strata = comuna)
+dt_split <- initial_split(propie, p = 3/4, strata = comuna)
 dt_training <- training(dt_split)
 dt_testing <- testing(dt_split)
 #model
@@ -27,18 +27,19 @@ lm_model <- linear_reg() %>%
   set_engine('lm') %>% # adds lm implementation of linear regression
   set_mode('regression')
 lm_fit <- lm_model %>%
-  fit(tot_constr_m2 ~ calidad_bien_ppal +bien_casa + total_uf, data = dt_training)
+  fit(total_uf ~ tot_constr_m2 + tot_terreno_m2 + num_estacionam, 
+      data = dt_training)
  summary(lm_fit$fit)
 vip(lm_fit) 
 glance(lm_fit)
 m1 <- rand_forest(trees = 3000) %>%
   set_engine("ranger") %>% 
   set_mode("regression")
-# m2 <- linear_reg() %>% 
-#   set_engine('lm') %>% 
-#   set_mode('regression')
+m2 <- linear_reg() %>%
+  set_engine('lm') %>%
+  set_mode('regression')
 #set recipe ----
-m_recipe <- recipe(tot_constr_m2 ~ calidad_bien_ppal + bien_casa + total_uf, data = dt_training) %>%
+m_recipe <- recipe(total_uf ~ tot_constr_m2 + tot_terreno_m2 + num_estacionam, data = dt_training) %>%
   step_dummy(all_nominal(), -all_outcomes()) %>%
   step_zv(all_predictors()) %>%
   step_normalize(all_predictors())
@@ -60,10 +61,19 @@ summary(m_fit$fit$fit$fit)
 #prediction ----
 m_train_pred <- predict(m_fit, dt_training) %>%
   bind_cols(dt_training)
-ggplot(aes(.pred),data = m_train_pred) +
+ggplot(aes(.pred), data = m_train_pred) +
   geom_histogram()
-select(m_train_pred, tot_constr_m2, .pred) %>%
-  ggplot(aes(.pred, tot_constr_m2)) +
+select(m_train_pred, total_uf, .pred) %>%
+  ggplot(aes(.pred, total_uf)) +
+  geom_point() + 
+  geom_smooth(formula= y~x)
+
+m_test_pred <- predict(m_fit, dt_testing) %>%
+  bind_cols(dt_testing)
+ggplot(aes(.pred), data = m_test_pred) +
+  geom_histogram()
+select(m_test_pred, total_uf, .pred) %>%
+  ggplot(aes(.pred, total_uf)) +
   geom_point() + 
   geom_smooth(formula= y~x)
 ####################aplicar técnicas de vaidación cruzada
